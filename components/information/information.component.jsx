b60271bb7d6d165e57b7d705e21f91f09d8588bc
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-03-13 21:48:03 +0100

    feat: information with proyects wip

--- components/information/information.component.jsx
+++ components/information/information.component.jsx
@@ -3,6 +3,7 @@ import AboutMeComponent from './aboutMe/aboutMe.component';
 import StudiesComponent from './studies/studies.component';
 import ExperiencesComponent from './experiences/experiences.component';
 import SkillsComponent from './skills/skills.component';
+import ProyectsComponent from './proyects/proyects.component';
 
 export default function InformationComponent() {
     return (
@@ -11,6 +12,7 @@ export default function InformationComponent() {
             <StudiesComponent />
             <ExperiencesComponent />
             <SkillsComponent />
+            <ProyectsComponent />
         </section>
     )
 }
---
+++ components/information/proyects/proyects.component.jsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import style from './proyects.module.scss';
+
+export default function ProyectsComponent() {
+    return (
+        <article className={style.article}>
+            <div className={style.title_article}>Proyectos</div>
+            <div className={style.data_article_container}>
+                <div className={style.data_article}>
+                    <Image
+                        width={500}
+                        height={500}
+                        alt={'Imagen de proyecto'}
+                        src={'/images/proyecto_needbeatthedragon.jpg'}
+                    />
+                    <div>Need Beat the Dragon</div>
+                    <div className={style.text_article}>Videojuego en Javascript</div>
+                </div>
+                <div className={style.data_article}>
+                    <Image
+                        width={500}
+                        height={500}
+                        alt={'Imagen de proyecto'}
+                        src={'/images/proyecto_appvideoclub.jpg'}
+                    />
+                    <div>Videoclub</div>
+                    <div className={style.text_article}>APP en React y Express</div>
+                </div>
+            </div>
+        </article>
+    )
+}
