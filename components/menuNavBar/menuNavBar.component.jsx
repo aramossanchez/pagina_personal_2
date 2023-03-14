d8e45a158970014d11404f7004cda7e20647dfe6
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-03-15 00:59:13 +0100

    feat: can change theme :)

--- components/menuNavBar/menuNavBar.component.jsx
+++ components/menuNavBar/menuNavBar.component.jsx
@@ -1,22 +1,46 @@
+import { useState } from 'react';
 import style from './menuNavBar.module.scss';
 
 export default function MenuNavBarComponent() {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const handleThemeChangeLight = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('light_theme');
+    };
+
+    const handleThemeChangeFruit = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('fruit_theme');
+    };
+
+    const handleThemeChangeRainy = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('rainy_theme');
+    };
+
+    const handleThemeChangeEight = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('eight_theme');
+    };
+
     return (
         <section className={style.menu_navbar_container}>
             <div className={style.link}>
-                <span className="material-icons">person_outline</span>
+                <span className="material-icons" onClick={() => handleThemeChangeLight()}>person_outline</span>
                 <span>Sobre mí</span>
             </div>
             <div className={style.link_active}>
-                <span className="material-icons">school</span>
+                <span className="material-icons" onClick={() => handleThemeChangeFruit()}>school</span>
                 <span>Estudios</span>
             </div>
             <div className={style.link}>
-                <span className="material-icons">apartment</span>
+                <span className="material-icons" onClick={() => handleThemeChangeRainy()}>apartment</span>
                 <span>Experiencia</span>
             </div>
             <div className={style.link}>
-                <span className="material-icons">calendar_view_month</span>
+                <span className="material-icons" onClick={() => handleThemeChangeEight()}>calendar_view_month</span>
                 <span>Skills</span>
             </div>
             <div className={style.link}>
