b7204a9959acfd93b9a272b40a0c6defb51b9b1d
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-06-05 00:00:31 +0200

    feat: selector de tema

--- components/profile/profile.component.jsx
+++ components/profile/profile.component.jsx
@@ -1,12 +1,43 @@
 import Image from 'next/image';
 import style from './profile.module.scss';
 import { IconBrandDiscord, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandSteam, IconBrandTwitter, IconRollerSkating } from '@tabler/icons-react';
+import { useState } from 'react';
 
 
 export default function ProfileComponent() {
+
+    const [activeTheme, setActiveTheme] = useState('primary_theme');
+    const [openedThemeSelector, setOpenedThemeSelector] = useState(null);
+
+    const themes = ['primary_theme', 'light_theme', 'fruit_theme', 'rainy_theme', 'eight_theme'];
+
+    const handleThemeChangeTheme = (theme) => {
+        themes.forEach(themeOfArray => {
+            if (themeOfArray !== theme) {
+                document.body.classList.remove(themeOfArray);
+            }
+        });
+        document.body.classList.toggle(theme);
+        setActiveTheme(theme);
+    };
+
     return (
         <section className={style.profile_container}>
             <div className={style.background_image_container}>
+                <div className={style.open_selector_theme} onClick={() => setOpenedThemeSelector(true)}>
+                    <span className='material-icons'>brush</span>
+                </div>
+                <div className={`${style.color_selector} ${openedThemeSelector === null ? '' : openedThemeSelector ? style.selector_open : style.selector_close}`}>
+                    {themes.map((theme) => {
+                        return (
+                            <div
+                                key={theme}
+                                className={`${style.color_button_selector} ${style[theme]} ${theme === activeTheme ? style.theme_selected : ''}`}
+                                onClick={() => { handleThemeChangeTheme(theme); setOpenedThemeSelector(false) }}>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <div className={style.profile_photo_container}>
                 <Image
@@ -18,7 +49,7 @@ export default function ProfileComponent() {
             </div>
             <div className={style.information_profile_container}>
                 <span className={style.information_profile_name}>Armando Ramos</span>
-                <span className={style.information_profile_role}>Front-end Developer</span>
+                <span className={style.information_profile_role}>Full Stack Developer</span>
                 <div className={style.information_profile_social_networks_container}>
                     <a target="_blank" href='https://github.com/aramossanchez' rel="noopener noreferrer">
                         <IconBrandGithub />
