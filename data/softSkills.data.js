ffaab13736b1cd604ceecb997e460044efe5fe20
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-10-12 09:57:01 +0200

    feat: traducción en skills

--- data/softSkills.data.js
+++ data/softSkills.data.js
@@ -2,52 +2,52 @@ const softSkills = [
   {
     Image: 'images/communication_icon.png',
     Text: 'Communication',
-    Label: 'Naturalidad hablando a través de videollamada o llamada telefónica con compañeros de equipo y con clientes.'
+    Label: 'Ease of speaking via video call or telephone call with team members and clients.'
   },
   {
     Image: 'images/troubleshooting_icon.png',
     Text: 'Troubleshooting',
-    Label: ''
+    Label: 'Ability to locate the source of bugs and errors.'
   },
   {
     Image: 'images/selfmanagement_icon.png',
     Text: 'Self management',
-    Label: ''
+    Label: 'Ability to self-manage my tasks and duties at the workplace.'
   },
   {
     Image: 'images/curiosity_icon.png',
     Text: 'Curiosity',
-    Label: ''
+    Label: 'Interest in continuing to learn about everything related to the world of programming.'
   },
   {
     Image: 'images/workteam_icon.png',
     Text: 'Workteam',
-    Label: ''
+    Label: 'Ease of working on projects where all members collaborate towards a common goal.'
   },
   {
     Image: 'images/fastlearning_icon.png',
     Text: 'Fast learning',
-    Label: ''
+    Label: 'Quickly learn new technologies and methods to improve in my job.'
   },
   {
     Image: 'images/empathy_icon.png',
     Text: 'Empathy',
-    Label: ''
+    Label: 'Connecting with team members on a deeper level to understand their perspectives, ensuring smooth collaboration and mutual respect.'
   },
   {
     Image: 'images/organization_icon.png',
     Text: 'Organization',
-    Label: ''
+    Label: 'Accustomed to managing my time and schedules to efficiently complete tasks and projects.'
   },
   {
     Image: 'images/changes_icon.png',
     Text: 'Adaptation to changes',
-    Label: ''
+    Label: 'Prepared for changes in the work mode, technologies in a project, or members of a work team.'
   },
   {
     Image: 'images/acceptcriticism_icon.png',
     Text: 'Accept criticism',
-    Label: ''
+    Label: 'Aware of the constant ability to improve and prepared to accept criticism about my work, to achieve the best version of myself.'
   }
 ];
 
