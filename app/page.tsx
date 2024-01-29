821786500f3c28405f1fb670494f165d7d7a3c8b
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2024-01-29 10:54:47 +0100

    feat: adornos acabados

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { OrnamentsContainer } from '@/containers/Ornaments/Ornaments.container'
 
 export default function Index() {
   return (
-    <div className='relative overflow-x-hidden z-0'>
+    <div className='relative overflow-x-hidden overflow-y-hidden z-0'>
       <OrnamentsContainer />
       <main className='w-[100vw] overflow-x-hidden'>
         <MenuTopContainer />
--- containers/Ornaments/Ornaments.container.tsx
+++ containers/Ornaments/Ornaments.container.tsx
@@ -15,6 +15,35 @@ export function OrnamentsContainer() {
         absolute bg-gradient-to-r from-primaryColor1 to-primaryColor2 left-[-4%] top-[95vh] leading-none opacity-50 origin-center -rotate-[20deg]
       `}></span>
       <span className='absolute w-[20vw] h-[20vw] border-8 border-primaryColor2 right-[-10%] top-[90vh] leading-none origin-center -rotate-[40deg]'></span>
+      <span className={`
+        ${style.triangle_animated_index_presentation_first}
+        w-[20vw] h-[20vw] absolute bg-gradient-to-r from-primaryColor1 to-primaryColor2 left-[-6%] top-[150vh] leading-none opacity-50 origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.bubble_animated_index_presentation_second}
+        w-[10vw] h-[10vw] rounded-full absolute border-8 border-primaryColor2 right-[-3%] top-[250vh] leading-none origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.square_animated_index_presentation_second}
+        w-[20vw] h-[20vw] absolute border-8 border-primaryColor2 left-[-10%] top-[350vh] leading-none origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.bubble_animated_index_presentation_third}
+        w-[10vw] h-[10vw] rounded-full absolute bg-gradient-to-r from-primaryColor1 to-primaryColor2 left-[-6%] top-[550vh] leading-none opacity-50 origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.square_animated_index_presentation_third}
+        min-[1023px]:w-[10vw] w-[100px] min-[1023px]:h-[10vw] h-[100px]
+        absolute bg-gradient-to-r from-primaryColor1 to-primaryColor2 right-[-4%] top-[705vh] leading-none opacity-50 origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.square_animated_index_presentation_fourth}
+        w-[40vw] h-[40vw] border-4 border-primaryColor2 absolute left-[-6%] top-[900vh] opacity-30 leading-none origin-center -rotate-[20deg]
+      `}></span>
+      <span className={`
+        ${style.triangle_animated_index_presentation_second}
+        w-[20vw] h-[20vw] absolute bg-gradient-to-r from-primaryColor1 to-primaryColor2 right-[-6%] top-[1050vh] leading-none opacity-50 origin-center -rotate-[20deg]
+      `}></span>
     </>
   )
 }
